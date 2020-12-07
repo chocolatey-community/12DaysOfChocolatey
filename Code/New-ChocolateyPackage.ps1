21160b9951a5c142bd3f6b4ed62ce2437e49a01a
steviecoaster <stephen@chocolatey.io>
2020-12-06 16:30:01 -0800

    initial commit

---
+++ Code/New-ChocolateyPackage.ps1
@@ -0,0 +1,73 @@
+function New-ChocolateyPackage {
+    <#
+    .SYNOPSIS
+    Generates a Chocolatey package from an installer or a URL
+    
+    .DESCRIPTION
+    Quickly create a Chocolatey Package from a file or URL. Requires a Chocolatey For Business license, and the chocolatey.extension package
+    
+    .PARAMETER File
+    The local installer (msi, or exe) to generate a Chococlatey Package from
+    
+    .PARAMETER Url
+    The URL to download a binary to turn into a Chocolatey Package
+    
+    .PARAMETER OutputDirectory
+    The finished Chocolatey Package location
+    
+    .EXAMPLE
+    New-ChocolateyPackage -File C:\installers\7-zip.msi -OutputDirectory C:\ChocolateyPackages
+
+    .EXAMPLE
+    New-ChocolateyPackage -File C:\installers\7-zip.msi,C:\installers\npp.exe -OutputDirectory C:\ChocolateyPackages
+
+    New-ChocolateyPackage -File ((Get-ChildItem \\server\installers -INclude *.exe,*.msi).Fullname) -OutputDirectory C:\ChocolateyPackages
+    .EXAMPLE
+    New-ChocolateyPackage -Url 'https://dl.google.com/ent/googlechrome64.msi' -OutputDirectory C:\ChocolateyPackages
+    
+    .NOTES
+    General notes
+    #>
+    [cmdletBinding(DefaultParameterSetname="file")]
+    Param(
+        [Parameter(ParameterSetName="File",Mandatory)]
+        [String[]]
+        $File,
+
+        [Parameter(ParameterSetName="url",Mandatory)]
+        [String[]]
+        $Url,
+
+        [Parameter(ParameterSetName="file",Mandatory)]
+        [Parameter(ParameterSetName="url",Mandatory)]
+        [String]
+        $OutputDirectory
+    )
+
+    process {
+
+        if(-not (Test-Path $OutputDirectory)){
+            $null = New-Item $OutputDirectory -ItemType Directory
+        }
+
+        switch($PSCmdlet.ParameterSetName){
+            'file' {
+                Foreach($f in $File){
+
+                    $chocoArgs = @('new',"--file='$f'",'--build-package',"--output-directory='$OutputDirectory'")
+                    & choco @chocoArgs
+
+                }
+            }
+
+            'url' {
+                foreach($u in $Url){
+
+                    $chocoArgs = @('new',"--url='$u'",'--build-package',"--output-directory='$OutputDirectory'")
+                    & choco @chocoArgs
+                }
+
+            }
+        }
+    }
+}
---
+++ Examples/visualstudio-iso/visualstudio/tools/chocolateyinstall.ps1
@@ -0,0 +1,95 @@
+﻿#Define some package metadata
+$packageName = 'visualstudio'
+$fileType = 'exe'
+$checksum = '4E1F4D8389A3565DFCFDA6AEB1F2FA4364E96D20A2C3B3CB6D12B7146EA1E978'
+$checksumType = 'sha256'
+$url = 'http://nexus.fabrikam.com:8081/repository/installers/vslayout.iso'
+
+#define where to store the ISO on disk once downloaded
+$isoFile = Split-Path $url -Leaf
+
+$downloadPath = "$env:TEMP\$env:ChocolateyPackageName\$env:CHocolateyPackageVersion"
+
+if(-not (Test-Path $downloadPath)){
+    $null = New-Item $downloadPath -ItemType Directory
+}
+
+$isoPath = Join-Path $downloadPath $isoFile
+
+#Set arguments and call Get-ChocolateyWebFile. Splatting FTW!
+$downloadArgs = @{
+    PackageName = $packageName
+    FileFullPath = $isoPath
+    Url = $url
+    Checksum = $checksum
+    ChecksumType = $checksumType
+}
+
+Get-ChocolateyWebFile @downloadArgs
+
+#Mount Downloaded ISO, Gather drive latter, Set our shell location, and collect setup file
+$null = Mount-DiskImage -ImagePath $isoPath
+$installPath = ((Get-Volume -FileSystemLabel 'vslayout').DriveLetter) + ":"
+Push-Location $installPath
+$installer = Join-Path $installPath 'vs_setup.exe'
+
+#Install all the certificates for Visual Studio. It's just PowerShell in here, you can do any PowerShell-y things you want.
+
+<# One way to do it:
+start-process .\certmgr.exe -ArgumentList '-add -c certificates\manifestSignCertificates.p12 -n "Microsoft Code Signing PCA 2011" -s -r LocalMachine CA' -Wait -NoNewWindow
+start-process .\certmgr.exe -ArgumentList '-add -c certificates\manifestSignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root' -Wait -NoNewWindow
+start-process .\certmgr.exe -ArgumentList '-add -c certificates\manifestCounterSignCertificates.p12 -n "Microsoft Time-Stamp PCA 2010" -s -r LocalMachine CA' -Wait -NoNewWindow
+start-process .\certmgr.exe -ArgumentList '-add -c certificates\manifestCounterSignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root' -Wait -NoNewWindow
+start-process .\certmgr.exe -ArgumentList '-add -c certificates\vs_installer_opc.SignCertificates.p12 -n "Microsoft Code Signing PCA" -s -r LocalMachine CA' -Wait -NoNewWindow
+start-process .\certmgr.exe -ArgumentList '-add -c certificates\vs_installer_opc.SignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root' -Wait -NoNewWindow
+#>
+
+@(
+    @{
+        ArgumentList = '-add -c certificates\manifestSignCertificates.p12 -n "Microsoft Code Signing PCA 2011" -s -r LocalMachine CA'
+        Wait = $true
+        NoNewWindow = $true
+    },
+    @{
+        ArgumentList = '-add -c certificates\manifestSignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root'
+        Wait = $true
+        NoNewWindow = $true
+
+    },
+    @{
+        ArgumentList = '-add -c certificates\manifestCounterSignCertificates.p12 -n "Microsoft Time-Stamp PCA 2010" -s -r LocalMachine CA'
+        Wait = $true
+        NoNewWindow = $true
+
+    },
+    @{
+        ArgumentList = '-add -c certificates\manifestCounterSignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root'
+        Wait = $true
+        NoNewWindow = $true
+
+    },
+    @{
+        ArgumentList = '-add -c certificates\vs_installer_opc.SignCertificates.p12 -n "Microsoft Code Signing PCA" -s -r LocalMachine CA'
+        Wait = $true
+        NoNewWindow = $true
+
+    },
+    @{
+        ArgumentList = '-add -c certificates\vs_installer_opc.SignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root'
+        Wait = $true
+        NoNewWindow = $true
+    }
+) | ForEach-Object { & .\certmgr.exe @$_}
+
+#All that, and we can _finally_ install Visual Studio
+$installerArgs = @{
+    statements = '--noweb --passive --wait --norestart'
+    exe2run = $installer
+}
+
+Start-ChocolateyProcessAsAdmin @installerArgs
+
+#Give us our shell back, and cleanup
+Pop-Location
+Dismount-DiskImage -ImagePath $isoPath
+Remove-Item $downloadPath -Recurse -Force
