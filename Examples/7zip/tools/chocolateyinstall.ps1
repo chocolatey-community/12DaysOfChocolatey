4d1a7fdbb5f7f6a21232587fc50ea9342b29cd73
steviecoaster <stephen@chocolatey.io>
2020-12-06 16:42:26 -0800

    Add 7zip example

---
+++ Examples/7zip/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  file         = $fileLocation
+  softwareName  = '7zip*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '' #(Get-FileHash $installer).Hash (Defaults to sha256)
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
